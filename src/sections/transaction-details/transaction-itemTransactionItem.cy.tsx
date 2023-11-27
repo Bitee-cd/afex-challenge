50290728ffe215beb76445ce269f06a76ea5e566
Bitee-cd <calebduniya45@gmail.com>
2023-11-27 05:48:58 +0100

    removed stale cypress component

--- src/sections/transaction-details/transaction-itemTransactionItem.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import TransactionItem from './transaction-item'
-
-describe('<TransactionItem />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<TransactionItem />)
-  })
-})
